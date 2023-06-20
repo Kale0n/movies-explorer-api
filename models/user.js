ba22ccc74b86c42322bc60cfdf84fa32eda788f4
Kaleon <Kaleon612@gmail.com>
2023-06-20 16:31:34 +0300

    name not reqiured + unique email

--- models/user.js
+++ models/user.js
@@ -8,9 +8,9 @@ const INVALID_CREDENTIALS_MESSAGE = 'Неправильные почта или 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Эмир Кустурица',
   },
   email: {
     type: String,
